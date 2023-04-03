9f6522ca9d060ad26f8ddd64589e019204aa5596
Denys Shevel <denys.shevel16@gmail.com>
2023-04-03 09:50:42 +0200

    Fix dates of the schedule calendar

--- frontend/src/utils/getShortDayOfWeekByIndex.js
+++ frontend/src/utils/getShortDayOfWeekByIndex.js
@@ -1,7 +1,7 @@
 export function getShortDateOfWeekByIndex(index) {
   const dateObject = new Date();
 
-  dateObject.setDate(dateObject.getDate() + dateObject.getDay() - index);
+  dateObject.setDate(dateObject.getDate() - dateObject.getDay() + index);
 
   return dateObject.toLocaleDateString('default', {
     weekday: 'short',
